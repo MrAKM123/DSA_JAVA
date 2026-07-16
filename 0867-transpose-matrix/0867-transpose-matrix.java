8c4f05cf1b077d6fd430333d343942f4872e4822
Akash Maurya <akashmaurya2905@gmail.com>
2026-07-16 13:39:00 +0530

    Time: 1 ms (26.28%), Space: 46.8 MB (27.82%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-       if(matrix == null || matrix.length==0){
-        return new int[0][0];
-       } 
-      //for original matrix
-      int totalRow=matrix.length;
-      int totalColumn= matrix[0].length;
-      // for tanspose matrix
-      int newtotalRow =totalColumn;
-      int newtoalColumn= totalRow;
-
-      int newMatrix[][]= new int[totalColumn][totalRow] ;
-
-      // Actuall logic
-       for(int i=0; i<totalRow; i++){
-        for(int j=0; j<totalColumn; j++){
-            newMatrix[j][i] = matrix[i][j];
+        if(matrix == null || matrix.length == 0){
+            return new int[0][0];
         }
-       } 
+          
+          int totalRow =matrix.length;
+          int totalColoumn =matrix[0].length;
 
-      return newMatrix;
+          int newTotalRow= totalColoumn;
+          int newTotalColoumn= totalRow ;
 
+          int newMatrix[][]= new int[ newTotalRow][newTotalColoumn];
+         for( int i= 0; i<matrix.length; i++){
+            for(int j=0; j< matrix[0].length; j++){
+            newMatrix[j][i]= matrix[i][j]; 
+            }
+         }
+
+        return  newMatrix;
     }
 }
