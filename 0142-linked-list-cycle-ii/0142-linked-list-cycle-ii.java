dca6a1f19d6120a9a1b9f87addde57abb6c17e2e
Akash Maurya <akashmaurya2905@gmail.com>
2026-08-25 21:35:17 +0530

    Time: 0 ms (100%), Space: 46.3 MB (94.03%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,20 +9,49 @@
  *     }
  * }
  */
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-       if(head == null || head.next == null ){
-        return null;
-       } 
-       HashSet<ListNode> set  = new HashSet<>();
-       ListNode temp = head;
-       while(temp != null){
-        if(set.contains(temp)){
-            return temp;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        Boolean hasCyclic = false;
+        // detect cyclic
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                hasCyclic = true;
+                break;
+            }
         }
-         set.add(temp);
-         temp =temp.next;
-       }
-       return temp;
+        if (hasCyclic == false) {
+            return null;
+        }
+        // cyclic head
+        // slow ko head pr rakho aur fast jaha h wahi rhega
+        // slow ansd fast ko ek ek step age bada
+        // when they meet rhe node is starting of that cyclic
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+
+        //  // using set complexity
+        //        if(head == null || head.next == null ){
+        //         return null;
+        //        } 
+        //        HashSet<ListNode> set  = new HashSet<>();
+        //        ListNode temp = head;
+        //        while(temp != null){
+        //         if(set.contains(temp)){
+        //             return temp;
+        //         }
+        //          set.add(temp);
+        //          temp =temp.next;
+        //        }
+        //        return temp;
     }
 }
